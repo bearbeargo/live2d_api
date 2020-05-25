a6d134cb597fb3331e4fbb5697ded7343c2cb799
Debian Bear <debianbear@gmail.com>
2020-05-25 13:35:56 +0800

    为live2d图片添加cdn地址

--- get/index.php
+++ get/index.php
@@ -10,36 +10,46 @@ $modelTextures = new modelTextures();
 $jsonCompatible = new jsonCompatible();
 
 $id = explode('-', $id);
-$modelId = (int)$id[0];
-$modelTexturesId = isset($id[1]) ? (int)$id[1] : 0;
+$modelId = (int) $id[0];
+$modelTexturesId = isset($id[1]) ? (int) $id[1] : 0;
 
 $modelName = $modelList->id_to_name($modelId);
 
 if (is_array($modelName)) {
-    $modelName = $modelTexturesId > 0 ? $modelName[$modelTexturesId-1] : $modelName[0];
-    $json = json_decode(file_get_contents('../model/'.$modelName.'/index.json'), 1);
+    $modelName = $modelTexturesId > 0 ? $modelName[$modelTexturesId - 1] : $modelName[0];
+    $json = json_decode(file_get_contents('../model/' . $modelName . '/index.json'), 1);
 } else {
-    $json = json_decode(file_get_contents('../model/'.$modelName.'/index.json'), 1);
+    $json = json_decode(file_get_contents('../model/' . $modelName . '/index.json'), 1);
     if ($modelTexturesId > 0) {
         $modelTexturesName = $modelTextures->get_name($modelName, $modelTexturesId);
         if (isset($modelTexturesName)) $json['textures'] = is_array($modelTexturesName) ? $modelTexturesName : array($modelTexturesName);
     }
 }
 
+$cdn_img = 'https://img.b.cr/bearbeargo/live2d-api/model/';
+$cdn_others = ''; // There is something wrong with others cdn
+$img_path = '../model/';
+$others_path = '../model/';
+if ($cdn_img != '') {
+    $img_path = $cdn_img;
+}
+if ($cdn_others != '') {
+    $others_path = $cdn_others;
+}
 foreach ($json['textures'] as $k => $texture)
-	$json['textures'][$k] = '../model/' . $modelName . '/' . $texture;
+    $json['textures'][$k] = $img_path . $modelName . '/' . $texture;
 
-$json['model'] = '../model/'.$modelName.'/'.$json['model'];
-if (isset($json['pose'])) $json['pose'] = '../model/'.$modelName.'/'.$json['pose'];
-if (isset($json['physics'])) $json['physics'] = '../model/'.$modelName.'/'.$json['physics'];
+$json['model'] = '../model/' . $modelName . '/' . $json['model'];
+if (isset($json['pose'])) $json['pose'] = $others_path . $modelName . '/' . $json['pose'];
+if (isset($json['physics'])) $json['physics'] = $others_path . $modelName . '/' . $json['physics'];
 
 if (isset($json['motions']))
-    foreach ($json['motions'] as $k => $v) foreach($v as $k2 => $v2) foreach ($v2 as $k3 => $motion)
-        if ($k3 == 'file' || $k3 == 'sound') $json['motions'][$k][$k2][$k3] = '../model/' . $modelName . '/' . $motion;
+    foreach ($json['motions'] as $k => $v) foreach ($v as $k2 => $v2) foreach ($v2 as $k3 => $motion)
+        if ($k3 == 'file' || $k3 == 'sound') $json['motions'][$k][$k2][$k3] = $others_path . $modelName . '/' . $motion;
 
 if (isset($json['expressions']))
-    foreach ($json['expressions'] as $k => $v) foreach($v as $k2 => $expression)
-        if ($k2 == 'file') $json['expressions'][$k][$k2] = '../model/' . $modelName . '/' . $expression;
+    foreach ($json['expressions'] as $k => $v) foreach ($v as $k2 => $expression)
+        if ($k2 == 'file') $json['expressions'][$k][$k2] = $others_path . $modelName . '/' . $expression;
 
 header("Content-type: application/json");
 echo $jsonCompatible->json_encode($json);
